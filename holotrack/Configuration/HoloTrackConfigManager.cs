de3de696bed49f08927d14389761a9b8b737b4e9
unknown <n20gaming2000@gmail.com>
2020-09-06 23:57:02 +0800

    setup configuration

---
+++ holotrack/Configuration/HoloTrackConfigManager.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using osu.Framework.Configuration;
+using osu.Framework.Graphics;
+using osu.Framework.Platform;
+using osuTK;
+
+namespace holotrack.Configuration
+{
+    public class HoloTrackConfigManager : IniConfigManager<HoloTrackSetting>
+    {
+        internal const string FILENAME = @"holotrack.ini";
+        protected override string Filename => FILENAME;
+
+        public HoloTrackConfigManager(Storage storage, IDictionary<HoloTrackSetting, object> defaultOverrides = null)
+            : base(storage, defaultOverrides)
+        {
+        }
+
+        protected override void InitialiseDefaults()
+        {
+            Set(HoloTrackSetting.BackgroundMode, BackgroundMode.Color);
+            Set(HoloTrackSetting.BackgroundColor, Colour4.Green);
+            Set(HoloTrackSetting.BackgroundImage, "");
+            Set(HoloTrackSetting.BackgroundScale, 1.0f, 1.0f, 5.0f, 0.1f);
+            Set(HoloTrackSetting.BackgroundPositionX, 0.0f);
+            Set(HoloTrackSetting.BackgroundPositionY, 0.0f);
+
+            Set(HoloTrackSetting.Model, "haru.haru.model3.json");
+            Set(HoloTrackSetting.ModelScale, 1.0f, 0.5f, 10.0f, 0.1f);
+            Set(HoloTrackSetting.ModelPositionX, 0.0f);
+            Set(HoloTrackSetting.ModelPositionY, 0.0f);
+
+            Set(HoloTrackSetting.MouseDrag, false);
+            Set(HoloTrackSetting.MouseWheel, false);
+        }
+    }
+
+    public enum HoloTrackSetting
+    {
+        BackgroundMode,
+        BackgroundColor,
+        BackgroundImage,
+        BackgroundScale,
+        BackgroundPositionX,
+        BackgroundPositionY,
+
+        Model,
+        ModelScale,
+        ModelPositionX,
+        ModelPositionY,
+
+        MouseDrag,
+        MouseWheel,
+    }
+
+    public enum BackgroundMode
+    {
+        Image,
+        Color,
+    }
+}
--- holotrack/HoloTrackGameBase.cs
+++ holotrack/HoloTrackGameBase.cs
@@ -1,13 +1,18 @@
+using holotrack.Configuration;
 using osu.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics.Cubism;
 using osu.Framework.Input;
 using osu.Framework.IO.Stores;
+using osu.Framework.Platform;
 
 namespace holotrack
 {
     public class HoloTrackGameBase : Game
     {
+        protected HoloTrackConfigManager LocalConfig;
+        protected Storage Storage { get; set; }
+
         private DependencyContainer dependencies;
         protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent) =>
             dependencies = new DependencyContainer(base.CreateChildDependencies(parent));
@@ -29,6 +34,8 @@ namespace holotrack
             var cameraManager = new CameraManager(Host.UpdateThread) { EventScheduler = Scheduler };
             dependencies.Cache(cameraManager);
 
+            dependencies.Cache(LocalConfig);
+
             AddFont(Resources, @"Fonts/NotoExtraCond");
             AddFont(Resources, @"Fonts/NotoExtraCond-Italic");
             AddFont(Resources, @"Fonts/NotoExtraCond-Light");
@@ -39,6 +46,14 @@ namespace holotrack
             AddFont(Resources, @"Fonts/NotoExtraCond-MediumItalic");
             AddFont(Resources, @"Fonts/NotoExtraCond-Black");
             AddFont(Resources, @"Fonts/NotoExtraCond-BlackItalic");
+        }
+
+        public override void SetHost(GameHost host)
+        {
+            base.SetHost(host);
+
+            Storage ??= host.Storage;
+            LocalConfig ??= new HoloTrackConfigManager(Storage);
         }
     }
 }
