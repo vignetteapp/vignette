6af7778bb6064e75edbe8e7e140552f5512c48e8
Nitrous <n20gaming2000@gmail.com>
2021-02-03 08:31:27 +0800

    setup abstract implementation

---
+++ Vignette.Application/Recognition/Face.cs
@@ -0,0 +1,15 @@
+﻿// Copyright 2020 - 2021 Vignette Project
+// Licensed under NPOSLv3. See LICENSE for details.
+
+using System.Collections.Generic;
+using osu.Framework.Graphics.Primitives;
+
+namespace Vignette.Application.Recognition
+{
+    public struct Face
+    {
+        public IEnumerable<FaceLandmark> Landmarks { get; set; }
+
+        public RectangleF Bounds { get; set; }
+    }
+}
---
+++ Vignette.Application/Recognition/FaceTracker.cs
@@ -0,0 +1,79 @@
+﻿// Copyright 2020 - 2021 Vignette Project
+// Licensed under NPOSLv3. See LICENSE for details.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using osu.Framework.Graphics;
+using Vignette.Application.Camera;
+
+namespace Vignette.Application.Recognition
+{
+    public abstract class FaceTracker : Component
+    {
+        public IReadOnlyList<Face> Faces => faces?.ToArray();
+
+        public int Tracked => faces?.Count() ?? 0;
+
+        public double Delta => Time.Current - lastUpdateTime;
+
+        protected ICamera Camera { get; private set; }
+
+        private readonly CancellationTokenSource cancellationToken = new CancellationTokenSource();
+
+        private bool isDisposed;
+
+        private double lastUpdateTime;
+
+        private IEnumerable<Face> faces;
+
+        private Task tracker;
+
+        public virtual void StartTracking(ICamera camera) => Camera = camera;
+
+        public virtual void StopTracking() => Camera = null;
+
+        protected abstract IEnumerable<Face> Track();
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+            tracker = Task.Factory.StartNew(() => track(cancellationToken.Token), cancellationToken.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+        }
+
+        private void track(CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                    return;
+
+                if (Camera?.Data == null)
+                    continue;
+
+                faces = Track();
+
+                lastUpdateTime = Time.Current;
+            }
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            base.Dispose(isDisposing);
+
+            if (isDisposed)
+                return;
+
+            if (!isDisposing)
+                return;
+
+            cancellationToken.Cancel();
+            tracker.Wait();
+            tracker.Dispose();
+            cancellationToken.Dispose();
+
+            isDisposed = true;
+        }
+    }
+}
