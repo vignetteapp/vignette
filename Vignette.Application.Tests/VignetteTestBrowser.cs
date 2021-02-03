d24dfe3ffc0f10f48b9e7d37d73e4e3dba2df373
Nitrous <n20gaming2000@gmail.com>
2021-02-03 17:17:23 +0800

    bootstrap visual tests

---
+++ Vignette.Application.Tests/VignetteTestBrowser.cs
@@ -0,0 +1,15 @@
+﻿// Copyright 2020 - 2021 Vignette Project
+// Licensed under NPOSLv3. See LICENSE for details.
+
+using osu.Framework.Testing;
+
+namespace Vignette.Application.Tests
+{
+    public class VignetteTestBrowser : VignetteApplicationBase
+    {
+        protected override void LoadComplete()
+        {
+            Add(new TestBrowser(@"Vignette"));
+        }
+    }
+}
