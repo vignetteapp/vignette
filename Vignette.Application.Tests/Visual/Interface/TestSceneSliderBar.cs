c0f03e5840197cef339d210cfebe31d255eefef6
Nitrous <n20gaming2000@gmail.com>
2021-02-15 19:36:58 +0800

    add tests

---
+++ Vignette.Application.Tests/Visual/Interface/TestSceneSliderBar.cs
@@ -0,0 +1,24 @@
+﻿// Copyright 2020 - 2021 Vignette Project
+// Licensed under NPOSLv3. See LICENSE for details.
+
+using osu.Framework.Bindables;
+using Vignette.Application.Graphics.Interface;
+
+namespace Vignette.Application.Tests.Visual.Interface
+{
+    public class TestSceneSliderBar : TestSceneInterface
+    {
+        public TestSceneSliderBar()
+        {
+            AddComponent(new VignetteSliderBar<float>
+            {
+                Width = 200,
+                Current = new BindableFloat
+                {
+                    MinValue = 0,
+                    MaxValue = 1,
+                }
+            });
+        }
+    }
+}
