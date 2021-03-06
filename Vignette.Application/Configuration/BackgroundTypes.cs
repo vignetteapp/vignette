678ea3b80569d871c18ca5f21fff87b9a0860218
Nathan Alo <n20gaming2000@gmail.com>
2021-03-06 19:22:49 +0800

    add config

---
+++ Vignette.Application/Configuration/BackgroundTypes.cs
@@ -0,0 +1,14 @@
+﻿// Copyright 2020 - 2021 Vignette Project
+// Licensed under NPOSLv3. See LICENSE for details.
+
+namespace Vignette.Application.Configuration
+{
+    public enum BackgroundTypes
+    {
+        Colour,
+
+        Image,
+
+        Video,
+    }
+}
---
+++ Vignette.Application/Configuration/DevelopmentApplicationConfigManager.cs
@@ -0,0 +1,17 @@
+﻿// Copyright 2020 - 2021 Vignette Project
+// Licensed under NPOSLv3. See LICENSE for details.
+
+using osu.Framework.Platform;
+
+namespace Vignette.Application.Configuration
+{
+    public class DevelopmentApplicationConfigManager : ApplicationConfigManager
+    {
+        protected override string Filename => @"app.dev.ini";
+
+        public DevelopmentApplicationConfigManager(Storage storage)
+            : base(storage)
+        {
+        }
+    }
+}
