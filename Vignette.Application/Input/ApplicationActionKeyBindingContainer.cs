563db4c9b9108c2b5d5a63c00a3c294522214119
Nathan Alo <n20gaming2000@gmail.com>
2021-03-06 19:23:37 +0800

    add keybindings

---
+++ Vignette.Application/Input/ApplicationActionKeyBindingContainer.cs
@@ -0,0 +1,21 @@
+﻿// Copyright 2020 - 2021 Vignette Project
+// Licensed under NPOSLv3. See LICENSE for details.
+
+using System.Collections.Generic;
+using osu.Framework.Input.Bindings;
+
+namespace Vignette.Application.Input
+{
+    public class ApplicationActionKeyBindingContainer : KeyBindingContainer<ApplicationAction>
+    {
+        public override IEnumerable<IKeyBinding> DefaultKeyBindings => new[]
+        {
+            new KeyBinding(new[] { InputKey.Escape }, ApplicationAction.ToggleNavigation),
+        };
+    }
+
+    public enum ApplicationAction
+    {
+        ToggleNavigation,
+    }
+}
--- Vignette.Application/VignetteApplicationBase.cs
+++ Vignette.Application/VignetteApplicationBase.cs
@@ -3,17 +3,22 @@
 
 using osu.Framework;
 using osu.Framework.Allocation;
+using osu.Framework.Development;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.IO.Stores;
 using osu.Framework.Platform;
+using Vignette.Application.Configuration;
 using Vignette.Application.Graphics.Themes;
+using Vignette.Application.IO;
 using Vignette.Application.Live2D.Resources;
 
 namespace Vignette.Application
 {
     public class VignetteApplicationBase : Game
     {
+        public bool IsDevelopmentBuild { get; }
+
         protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent)
             => dependencies = new DependencyContainer(base.CreateChildDependencies(parent));
 
@@ -25,8 +30,12 @@ namespace Vignette.Application
 
         protected Storage Storage { get; set; }
 
+        protected ApplicationConfigManager LocalConfig;
+
         public VignetteApplicationBase()
         {
+            IsDevelopmentBuild = DebugUtils.IsDebugBuild;
+
             AddRangeInternal(new[]
             {
                 new SafeAreaContainer
@@ -65,13 +74,28 @@ namespace Vignette.Application
             AddFont(Resources, @"Fonts/Raleway-Bold");
             AddFont(Resources, @"Fonts/Raleway-BoldItalic");
 
-            dependencies.Cache(new ThemeStore(Storage));
+            dependencies.CacheAs(this);
+            dependencies.CacheAs(LocalConfig);
+            dependencies.CacheAs(new ThemeStore(Storage));
+            dependencies.CacheAs(new ObservedVideoStore(Storage));
+            dependencies.CacheAs(new ObservedTextureStore(Storage));
         }
 
         public override void SetHost(GameHost host)
         {
             base.SetHost(host);
             Storage ??= host.Storage;
+
+            LocalConfig ??= IsDevelopmentBuild
+                ? new DevelopmentApplicationConfigManager(Storage)
+                : new ApplicationConfigManager(Storage);
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            base.Dispose(isDisposing);
+
+            LocalConfig?.Dispose();
         }
     }
 }
