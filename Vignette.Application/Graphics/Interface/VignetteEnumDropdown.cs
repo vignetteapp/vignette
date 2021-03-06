f4fe3a147fab9cf9d36a0986e39c48f7a02092e5
Nathan Alo <n20gaming2000@gmail.com>
2021-03-06 19:22:16 +0800

    add button enum variant

---
+++ Vignette.Application/Graphics/Interface/VignetteEnumDropdown.cs
@@ -0,0 +1,16 @@
+﻿// Copyright 2020 - 2021 Vignette Project
+// Licensed under NPOSLv3. See LICENSE for details.
+
+using System;
+
+namespace Vignette.Application.Graphics.Interface
+{
+    public class VignetteEnumDropdown<T> : VignetteDropdown<T>
+        where T : struct, Enum
+    {
+        public VignetteEnumDropdown()
+        {
+            Items = (T[])Enum.GetValues(typeof(T));
+        }
+    }
+}
