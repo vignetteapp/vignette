f6e7477ad945bf5e947a57dc3dca29a90c32ab1b
Nathan Alo <n20gaming2000@gmail.com>
2021-05-03 15:27:31 +0800

    add markdown support

---
+++ Vignette.Game.Tests/Visual/UserInterface/TestSceneMarkdown.cs
@@ -0,0 +1,46 @@
+// Copyright 2020 - 2021 Vignette Project
+// Licensed under NPOSLv3. See LICENSE for details.
+
+using System;
+using osu.Framework.Graphics;
+using Vignette.Game.Graphics.Containers;
+
+namespace Vignette.Game.Tests.Visual.UserInterface
+{
+    public class TestSceneMarkdown : UserInterfaceTestScene
+    {
+        public TestSceneMarkdown()
+        {
+            string text = string.Join(
+                Environment.NewLine,
+                "# Heading 1",
+                "## Heading 2",
+                "### Heading 3",
+                "#### Heading 4",
+                "##### Heading 5",
+                "###### Heading 6",
+                "",
+                "Paragraph",
+                "",
+                "**Bold**",
+                "*Italic*",
+                "1. Ordered Item",
+                "2. Ordered Item",
+                "",
+                "- Unordered Item",
+                "- Unordered Item",
+                "",
+                "[Link](https://github.com/vignette-project)",
+                "",
+                "![Image](https://avatars.githubusercontent.com/u/69518398)"
+            );
+
+            Add(new FluentMarkdownContainer
+            {
+                RelativeSizeAxes = Axes.X,
+                AutoSizeAxes = Axes.Y,
+                Text = text,
+            });
+        }
+    }
+}
---
+++ Vignette.Game/Graphics/Containers/FluentMarkdownContainer.cs
@@ -0,0 +1,83 @@
+// Copyright 2020 - 2021 Vignette Project
+// Licensed under NPOSLv3. See LICENSE for details.
+
+using Markdig.Syntax;
+using Markdig.Syntax.Inlines;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Containers.Markdown;
+using osu.Framework.Graphics.Cursor;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Platform;
+using Vignette.Game.Graphics.Sprites;
+using Vignette.Game.Graphics.Typesets;
+using Vignette.Game.Themeing;
+
+namespace Vignette.Game.Graphics.Containers
+{
+    public class FluentMarkdownContainer : MarkdownContainer
+    {
+        public override MarkdownTextFlowContainer CreateTextFlow()
+            => new ThemableMarkdownFlowContainer();
+
+        protected override MarkdownList CreateList(ListBlock listBlock)
+            => new ThemableMarkdownList();
+
+        protected class ThemableMarkdownFlowContainer : MarkdownTextFlowContainer
+        {
+            protected override SpriteText CreateSpriteText()
+            {
+                var themable = new ThemableSpriteText(false) { Colour = ThemeSlot.Gray190 };
+                Schedule(() => AddInternal(themable));
+
+                return themable.Create().With(d => d.Font = SegoeUI.Regular.With(size: 16));
+            }
+
+            protected override void AddLinkText(string text, LinkInline linkInline)
+                => AddDrawable(new ThemableMarkdownLinkText(text, linkInline));
+        }
+
+        protected class ThemableMarkdownList : MarkdownList
+        {
+            public ThemableMarkdownList()
+            {
+                Padding = new MarginPadding();
+            }
+        }
+
+        protected class ThemableMarkdownLinkText : CompositeDrawable, IHasTooltip
+        {
+            public string TooltipText => link;
+
+            private string text;
+
+            private string link;
+
+            public ThemableMarkdownLinkText(string text, LinkInline linkInline)
+            {
+                this.text = text;
+                this.link = linkInline.Url ?? string.Empty;
+            }
+
+            [BackgroundDependencyLoader]
+            private void load(GameHost host)
+            {
+                InternalChildren = new Drawable[]
+                {
+                    new ClickableContainer
+                    {
+                        AutoSizeAxes = Axes.Both,
+                        Action = () => host.OpenUrlExternally(link),
+                        Child = new ThemableSpriteText
+                        {
+                            Text = text,
+                            Font = SegoeUI.Regular.With(size: 16),
+                            Colour = ThemeSlot.AccentPrimary,
+                        }
+                    }
+                };
+            }
+        }
+    }
+}
