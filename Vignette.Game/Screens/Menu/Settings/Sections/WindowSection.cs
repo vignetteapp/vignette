fc5cb1782769c6620cc3def4b2684e30ecc7d34d
Nathan Alo <n20gaming2000@gmail.com>
2021-05-07 21:05:02 +0800

    fix window size not updating when window is unresizable

--- Vignette.Game/Screens/Menu/Settings/Sections/WindowSection.cs
+++ Vignette.Game/Screens/Menu/Settings/Sections/WindowSection.cs
@@ -74,6 +74,9 @@ namespace Vignette.Game.Screens.Menu.Settings.Sections
                 if (host.Window is SDL2DesktopWindow window)
                     window.Resizable = e.NewValue;
 
+                if (!e.NewValue)
+                    windowSize.TriggerChange();
+
                 windowModeSetting.Current.Disabled = e.NewValue;
                 resolutionWindowedSetting.Current.Disabled = e.NewValue;
                 resolutionFullscreenSetting.Current.Disabled = e.NewValue;
