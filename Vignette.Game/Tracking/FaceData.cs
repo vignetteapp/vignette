0614df08b98e0137ef5912289f03b48320d77201
Speykious <speykious@gmail.com>
2021-10-31 21:21:05 +0100

    OPEN THE EYES

--- Vignette.Game/Tracking/FaceData.cs
+++ Vignette.Game/Tracking/FaceData.cs
@@ -45,7 +45,7 @@ namespace Vignette.Game.Tracking
                 float va = euclidian_distance(leftEyeTopLeft, leftEyeBottomLeft);
                 float vb = euclidian_distance(leftEyeTopRight, leftEyeBottomRight);
                 float h = euclidian_distance(leftEyeLeft, leftEyeRight);
-                return aspect_ratio(va, vb, h);
+                return aspect_ratio(va, vb, h) * 2;
             }
         }
 
@@ -56,7 +56,7 @@ namespace Vignette.Game.Tracking
                 float va = euclidian_distance(rightEyeTopLeft, rightEyeBottomLeft);
                 float vb = euclidian_distance(rightEyeTopRight, rightEyeBottomRight);
                 float h = euclidian_distance(rightEyeLeft, rightEyeRight);
-                return aspect_ratio(va, vb, h);
+                return aspect_ratio(va, vb, h) * 2;
             }
         }
 
