072cc4ee5de77c7f8b7d7642cdfc7bbf75280b85
Nathan Alo <nathan.alo2000@gmail.com>
2023-07-14 21:31:55 +0800

    add `Window` node

---
+++ source/Vignette/Window.cs
@@ -0,0 +1,14 @@
+// Copyright (c) Cosyne
+// Licensed under GPL 3.0 with SDK Exception. See LICENSE for details.
+
+namespace Vignette;
+
+/// <summary>
+/// The root of <see cref="Vignette"/>.
+/// </summary>
+public sealed class Window : World
+{
+    internal Window()
+    {
+    }
+}
